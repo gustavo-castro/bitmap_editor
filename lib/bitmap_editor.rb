bce61b83eb3e56a9f0a4dfcd5eb3db1d852ba029
gustavo <gustavo.pdcastro@gmail.com>
2018-01-14 16:56:26 +0100

    Adding the I command, fixing the S command and creating a new test example

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -3,13 +3,26 @@ class BitmapEditor
   def run(file)
     return puts "please provide correct file" if file.nil? || !File.exists?(file)
 
+    image = ""
     File.open(file).each do |line|
       line = line.chomp
-      case line
-      when 'S'
+      case line[0]
+      when "I"
+        image = ""
+        m, n = line.match(/I ([0-9]+) ([0-9]+)/i).captures
+        m = m.to_i
+        n = n.to_i
+        for i in 1..n
+          image += "O"*m+"\n"
+        end
+      when "S"
+        if image.length == 0
           puts "There is no image"
+        else
+          puts image
+        end
       else
-          puts 'unrecognised command :('
+        puts 'unrecognised command :('
       end
     end
   end
